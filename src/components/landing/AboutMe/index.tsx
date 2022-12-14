2e6a4118afb5f930043cc274411283972589a814
Elliott Kan <elliottkan7@gmail.com>
2022-12-14 21:12:44 +1100

    Folders Updated

---
+++ src/components/landing/AboutMe/index.tsx
@@ -0,0 +1,16 @@
+import './style.scss'
+import Skill from './Skill'
+
+function Skills() {
+  return (
+    <div>
+    <Skill
+      name='Front-end'
+      detail='React, Typescript'
+      icon={reactIcon}
+    />
+    </div>
+  )
+}
+
+export default Skills
